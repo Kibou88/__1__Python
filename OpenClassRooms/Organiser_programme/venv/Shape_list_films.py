fede57f0609303de754c2d5837454da1d7f420a1
Satoshi <denki.satoshi@gmail.com>
2024-01-12 22:23:52 +0100

    update IHM Shape_list_films functions

--- OpenClassRooms/Organiser_programme/venv/Shape_list_films.py
+++ OpenClassRooms/Organiser_programme/venv/Shape_list_films.py
@@ -1,17 +1,29 @@
-def shape_list_films_all(list_films):
+def shape_list_films_all(list_films, list_film_shaped):
     """Fonction pour réunir toutes les fonctions permettant la mise en forme de la liste de films
     Avant: "Name (created_date)", "type"
     Après: "Name", "created_date", "type" """
-    separate_name_age(list_films)
+    created_date = []
+    name = []
+    type = []
 
-def separate_name_age(list_films):
-    """Fonction pour séparer le nom de la date de création du film"""
-    for name_created, type in enumerate(list_films):
-        full_info_film = list_films[name_created][0]
-        type = list_films[name_created][1]
-        print(f"Valeur de name_created: {full_info_film}")
-        print(f"Valeur de type {type}")
-        created_date = []
-        # for film in full_info_film:
-        created_date = full_info_film[-5:-1:1] # Récupère la date de création du film
-        print(f"Le film {full_info_film} a été créé le {created_date}")
+    ligne_film_shaped = [name, created_date, type]
+    for name_created, type_film in enumerate(list_films):
+        name, created_date = separate_name_date(list_films, name_created, name, created_date)  # Fonction pour séparer le nom
+        # de la date de création du film
+        type = shape_type(list_films, name_created, type)  # Fonction pour mettre en forme le type
+        list_film_shaped.append(ligne_film_shaped)
+
+    return name, created_date, type
+
+def shape_type(list_films, name_created, type):
+    """Fonction pour mettre sous format MAJUSCULE les types"""
+    type_film = list_films[name_created][1]
+    type.append(type_film.upper())
+    return type
+
+def separate_name_date(list_films, name_created, name, created_date):
+    """Fonction pour séparer le nom du film de la date de création"""
+    full_info_film = list_films[name_created][0]
+    name.append(full_info_film[0:len(full_info_film) - 7:1])  # Récupère le nom du film et supprime les espaces avant et après
+    created_date.append(full_info_film[-5:-1:1])  # Récupère la date de création du film
+    return name, created_date
