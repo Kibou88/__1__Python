d8b70702a5f8c7224af71168b70e4495d96148a2
Satoshi <denki.satoshi@gmail.com>
2024-01-30 22:49:41 +0100

    mouse caption code added

--- Coder_Facile/__2__Bot_pour_jeux_online/venv/QuickGrab.pyw
+++ Coder_Facile/__2__Bot_pour_jeux_online/venv/QuickGrab.pyw
@@ -1,6 +1,8 @@
 # QuickGrab.py
 #########################
 # Fait avec le magazine Coder facile #3
+# Valable sur le site de jeu:
+# https://www.crazygames.com/game/sushi-go-round
 #########################
 #/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
 # Les coordonnées ne sont valables que pour le PC Dell Inspiron 15
@@ -10,17 +12,14 @@
 from PIL import Image, ImageGrab
 import os
 import time
-from mouse_event import *
-
-# Variables
-x_pad = 359  # Coordonnées de x dans le coin supérieur gauche
-y_pad = 226  # Coordonnées de y dans le coin supérieur gauche
-x_pad2 = 1170 - 359
-y_pad2 = 833 - 226
 
 def screenGrab():
     """Fonction pour faire une copie d'écran en PNG de l'écran actuel"""
-
+    # Variables
+    x_pad = 359 #Coordonnées de x dans le coin supérieur gauche
+    y_pad = 226 #Coordonnées de y dans le coin supérieur gauche
+    x_pad2 = 1170-359
+    y_pad2 = 833-226
     box = (x_pad+1,y_pad+1,x_pad+x_pad2,y_pad+y_pad2) # Assigne un tuple à la variable "box"
     """ImageGrab.grab prend les paramètres suivants "ImageGrab.grab(x,y,x,y)":
         -   1er x,y => correspond au coin supérieur gauche
@@ -35,7 +34,7 @@ def screenGrab():
         partie "\\full__snap" + str(int(time.time()) + '.png': nomme le fichier"""
 
 def main():
-    get_cords(x_pad,y_pad)
+    screenGrab()
 
 if __name__ == '__main__':
     time.sleep(2)
---
+++ Coder_Facile/__2__Bot_pour_jeux_online/venv/mouse_event.py
@@ -0,0 +1,57 @@
+# mouse_event
+# But:
+# Permet de gérer toutes les actions de la souris
+##################################################
+
+# Package pywin32 à installer avant
+import win32api, win32con
+import time
+def left_click_down():
+    """Fonction pour appuyer sur le click  gauche
+    -------------------------------------
+    Fonction win32api.mouse_event(dwFlags, dx, dy, dwdata):
+        - dwFlags: Définit l'action de la souris
+            * win32con.MOUSEEVENTF_LEFTDOWN
+            *win32con.MOUSEEVENTF_LEFTUP
+            *win32con.MOUSEEVENTF_MIDDLEDOWN
+            *win32con.MOUSEEVENTF_MIDDLEUP
+            *win32con.MOUSEEVENTF_LEFTDOWN
+    """
+    # pos = win32api.GetCursorPos()
+    # time.sleep(2)
+    # new_pos = (pos[0]-50,pos[1]-50)
+    # win32api.SetCursorPos(new_pos)
+    # time.sleep(2)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) # Le bouton gauche de la souris est appuyé
+    time.sleep(1)
+#     print("click left down")
+
+def left_click_up():
+    """Fonction pour relâcher le click gauche de la souris"""
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) # Le bouton gauche de la souris est relaché
+    time.sleep(1)
+    # print("click gauche ok")
+
+def mouse_pos(cord,x_pad,y_pad):
+    """Fonction pour déplacer la souris
+    -----------------------------------
+    Param:
+        - cord: Liste qui contient les coordonnées sous la forme cord=(x,y)
+        - x_pad: Contient la coordonnée x du coin supérieur gauche
+        - y_pad: Contient la coordonnée y du coin supérieur gauche"""
+    win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
+
+def get_cords(x_pad,y_pad):
+    """Fonction pour récupérer l'emplacement de la souris
+    -----------------------------------
+    Param:
+        - x_pad: Contient la coordonnée x du coin supérieur gauche
+        - y_pad: Contient la coordonnée y du coin supérieur gauche"""
+    x,y=win32api.GetCursorPos()
+    x = x - x_pad
+    y = y - y_pad
+    print(x,y)
+
+
+if __name__ == "__main__":
+    left_click_down()
