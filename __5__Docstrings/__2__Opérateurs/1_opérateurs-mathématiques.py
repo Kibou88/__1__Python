3c8a9cfa481aeb262f16c0803c76891fde7024f6
Satoshi <denki.satoshi@gmail.com>
2024-02-08 22:08:30 +0100

    end of lessons about Opérateurs

---
+++ __5__Docstrings/__2__Opérateurs/1_opérateurs-mathématiques.py
@@ -0,0 +1,16 @@
+# Opérateurs mathématiques
+# - opérateurs simples et complexes
+##########################################
+
+#On peut les utiliser pour faire les chaînes
+print("Hello " + "world")
+print("Hello " * 3) # Affiche 3 fois "Hello"
+
+print("%: modulo récupère le reste de la division")
+print(10%2) # retourne 0
+print(6%4)
+print("// retourne le nombre entier")
+print(10/3)
+print(10//3)
+print("** puissance")
+print(2 **4)
