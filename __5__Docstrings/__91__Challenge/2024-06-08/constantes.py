01b617d5c898789feb6d9a4ff6dceb40473befc3
Satoshi <denki.satoshi@gmail.com>
2024-06-08 09:39:36 +0200

    Preparation folder challenge 2024-06-08

---
+++ __5__Docstrings/__91__Challenge/2024-06-08/constantes.py
@@ -0,0 +1,11 @@
+# constantes.py
+# But:
+# Contient les constantes du code
+# -----------------------------------
+# Date de création:
+# Date de dernière modification:
+# ------------------------------------------
+# version: 1.0
+# -
+#-------------------------------------------
+
