1d9674346ec267aebf963f6d18588bf867dec857
Satoshi <denki.satoshi@gmail.com>
2024-06-20 22:21:57 +0200

    V3 finished

--- __5__Docstrings/__91__Challenge/2024-06-08/venv/IHM/window.py
+++ __5__Docstrings/__91__Challenge/2024-06-08/venv/IHM/window.py
@@ -1,112 +1,112 @@
-    # window.py
-    # But:
-    # Contient le code du challenge
-    # -----------------------------------
-    # Date de création: 2024-06-08
-    # Date de dernière modification: 2024-06-18
-    # ------------------------------------------------------------------------------------------------
-    # version: 2.0
-    # - Importation du module complet tkintermapview + modif associé
-    # - Ajout et modification des coordonées GPS dans la partie "Constantes classe"
-    # - Ajout d'une constante "SPACING" permettant de faire les espaces entre chaque bouton + modif associé
-    # - Modification du zoom initial afin de voir tous les markers
-    # - Méthode "set_marker": ajout du ternaire dans "icon=" pour simplifier le code
-    #-------------------------------------------------------------------------------------------------
+# window.py
+# But:
+# Contient le code du challenge
+# -----------------------------------
+# Date de création: 2024-06-08
+# Date de dernière modification: 2024-06-20
+# ------------------------------------------------------------------------------------------------
+# version: 3.0
+# - Importation du module complet tkintermapview + modif associé (V2)
+# - Ajout et modification des coordonées GPS dans la partie "Constantes classe" (V2)
+# - Ajout d'une constante "SPACING" permettant de faire les espaces entre chaque bouton + modif associé (V2)
+# - Modification du zoom initial afin de voir tous les markers (V2)
+# - Méthode "set_marker": ajout du ternaire dans "icon=" pour simplifier le code (V2)
+# - Correction de la partie "Style" pour créer un style (V3)
+#-------------------------------------------------------------------------------------------------
 
-    # Appel des modules externes
-    from tkinter import ttk, Tk
-    from tkintermapview import TkinterMapView
-    import os
-    from PIL import Image, ImageTk
+# Appel des modules externes
+from tkinter import ttk, Tk
+from tkintermapview import TkinterMapView
+import os
+from PIL import Image, ImageTk
 
-    # Appel des modules internes
-    from constantes import DO_VINOS, CURRENT_PATH
+# Appel des modules internes
+from constantes import DO_VINOS, CURRENT_PATH
 
-    class MainWindow(Tk):
+class MainWindow(Tk):
+    """
+    Classe MainWindow:
+    Permet de définir toutes les caractéristiques de la fenêtre d'application
+    Hérite de la classe Tk
+
+    3 méthodes:
+     - set_marker: Pour la gestion des marqueurs sur la carte
+     - set_coordonnees: Pour gérer le centre de la carte avec la ville sélectionnée
+     - set_button: Pour gérer la création des boutons
+    :param
+    - Tk: Permet à MainWindow d'hériter de la classe Tk
+    """
+    # Constantes de la classe
+    POS_ROW = 10
+    POS_COLUMN = 25
+    WIDTH = 130
+    SPACING = 45
+        # Coordonnées GPS pour centrer la carte afin de voir tous les markers
+    POS_X = 38.4472452
+    POS_Y = -4.9252987
+
+    def __init__(self):
+        super().__init__()
+        # Windows options
+        self.title(f"VINOS IBEROS")
+        self.configure(bg="purple")
+        self.geometry('1000x700')
+
+        # create map widget
+        self.map_widget = TkinterMapView(self, width=745, height=650, corner_radius=0)
+        # set current widget position and zoom
+        self.map_widget.set_position(self.POS_X, self.POS_Y)  # Carte centrer pour faire apparaitre tous les markers
+        self.map_widget.set_zoom(5)
+        self.map_widget.place(x=230, y=25)
+
+        # Style options
+        self.style = ttk.Style(self)
+        self.style.theme_use('alt')
+        # Configure le style des boutons
+        self.style.configure('TButton',
+            borderwidth=5,
+            foreground="#FFCC00",
+            background="#663399",
+            padding=5)
+
+
+    def set_marker(self):
         """
-        Classe MainWindow:
-        Permet de définir toutes les caractéristiques de la fenêtre d'application
-        Hérite de la classe Tk
-
-        3 méthodes:
-         - set_marker: Pour la gestion des marqueurs sur la carte
-         - set_coordonnees: Pour gérer le centre de la carte avec la ville sélectionnée
-         - set_button: Pour gérer la création des boutons
-        :param
-        - Tk: Permet à MainWindow d'hériter de la classe Tk
+        Méthode pour afficher le type de vin en fonction de la ville
+        :return: Images (vin blanc ou rouge) sur les coordonnées GPS de la ville en fonction des valeurs du dictionnaires
         """
-        # Constantes de la classe
-        POS_ROW = 10
-        POS_COLUMN = 25
-        WIDTH = 130
-        SPACING = 45
-            # Coordonnées GPS pour centrer la carte afin de voir tous les markers
-        POS_X = 38.4472452
-        POS_Y = -4.9252987
+        blanco = ImageTk.PhotoImage(Image.open(os.path.join(CURRENT_PATH, "images", "blanco.webp")).resize((40, 40)))
+        tinto = ImageTk.PhotoImage(Image.open(os.path.join(CURRENT_PATH, "images", "tinto.webp")).resize((40, 40)))
+        for i in list(DO_VINOS):
+            self.map_widget.set_marker(DO_VINOS[i][0][0], DO_VINOS[i][0][1],
+                                       icon=(tinto if DO_VINOS[i][1].lower() == "tinto" else blanco))
 
-        def __init__(self):
-            super().__init__()
-            # Windows options
-            self.title(f"VINOS IBEROS")
-            self.configure(bg="purple")
-            self.geometry('1000x700')
+    def set_coordonnees(self, i):
+        """
+        Méthode pour centrer la carte sur la ville en fonction des coordonnées
+        :param i: Récupère le nom de la ville de la méthode set_button
+        :return: Permet de centrer la carte sur la ville sélectionnée
+        """
+        self.POS_X = DO_VINOS[i][0][0]
+        self.POS_Y = DO_VINOS[i][0][1]
+        self.map_widget.set_position(self.POS_X, self.POS_Y)  # Centrer sur la ville sélectionnée
+        self.map_widget.set_zoom(8) # Permet de faire un zoom sur la ville sélectionnée
 
-            # create map widget
-            self.map_widget = TkinterMapView(self, width=745, height=650, corner_radius=0)
-            # set current widget position and zoom
-            self.map_widget.set_position(self.POS_X, self.POS_Y)  # Carte centrer pour faire apparaitre tous les markers
-            self.map_widget.set_zoom(5)
-            self.map_widget.place(x=230, y=25)
-
-            # Style options
-            self.style = ttk.Style(self)
-            # Configure le style des boutons
-            self.style.configure('TButton',
-                font=('Helvitica', 10),
-                bd=10,
-                foreground="#FFCC00",
-                bg="#FFFF33",
-                activebackground="blue",
-                padding=5)
+    def set_button(self):
+        """
+        Méthode pour la création des boutons
+        :return: Créer tout les boutons du dictionnaire.
+        Action des boutons: Centrer la carte sur la ville sélectionnée
+        """
+        for i in list(DO_VINOS):
+            # Création de tout les boutons pour quitter la fenêtre avec position dans la grille
+            self.button = ttk.Button(self, text=i, command=lambda i=i:self.set_coordonnees(i))
+            self.button.place(x=self.POS_COLUMN, y=self.POS_ROW, width=self.WIDTH, height=40)
+            self.POS_ROW += self.SPACING
 
 
-        def set_marker(self):
-            """
-            Méthode pour afficher le type de vin en fonction de la ville
-            :return: Images (vin blanc ou rouge) sur les coordonnées GPS de la ville en fonction des valeurs du dictionnaires
-            """
-            blanco = ImageTk.PhotoImage(Image.open(os.path.join(CURRENT_PATH, "images", "blanco.webp")).resize((40, 40)))
-            tinto = ImageTk.PhotoImage(Image.open(os.path.join(CURRENT_PATH, "images", "tinto.webp")).resize((40, 40)))
-            for i in list(DO_VINOS):
-                self.map_widget.set_marker(DO_VINOS[i][0][0], DO_VINOS[i][0][1],
-                                           icon=(tinto if DO_VINOS[i][1].lower() == "tinto" else blanco))
-
-        def set_coordonnees(self, i):
-            """
-            Méthode pour centrer la carte sur la ville en fonction des coordonnées
-            :param i: Récupère le nom de la ville de la méthode set_button
-            :return: Permet de centrer la carte sur la ville sélectionnée
-            """
-            self.POS_X = DO_VINOS[i][0][0]
-            self.POS_Y = DO_VINOS[i][0][1]
-            self.map_widget.set_position(self.POS_X, self.POS_Y)  # Centrer sur la ville sélectionnée
-            self.map_widget.set_zoom(8) # Permet de faire un zoom sur la ville sélectionnée
-
-        def set_button(self):
-            """
-            Méthode pour la création des boutons
-            :return: Créer tout les boutons du dictionnaire.
-            Action des boutons: Centrer la carte sur la ville sélectionnée
-            """
-            for i in list(DO_VINOS):
-                # Création de tout les boutons pour quitter la fenêtre avec position dans la grille
-                self.button = ttk.Button(self, text=i, command=lambda i=i:self.set_coordonnees(i))
-                self.button.place(x=self.POS_COLUMN, y=self.POS_ROW, width=self.WIDTH, height=40)
-                self.POS_ROW += self.SPACING
-
-
-    if __name__ == "__main__":
-        main_window = MainWindow() # Créer l'instance main_window
-        main_window.set_marker() # Appel de la méthode pout afficher le type de vin en fonction de la ville
-        main_window.set_button() # Appel de la méthode pour créer et gérer les boutons
-        main_window.mainloop()  # Affiche la fenêtre
+if __name__ == "__main__":
+    main_window = MainWindow() # Créer l'instance main_window
+    main_window.set_marker() # Appel de la méthode pout afficher le type de vin en fonction de la ville
+    main_window.set_button() # Appel de la méthode pour créer et gérer les boutons
+    main_window.mainloop()  # Affiche la fenêtre
--- __5__Docstrings/__91__Challenge/2024-06-08/venv/Test/test.py
+++ __5__Docstrings/__91__Challenge/2024-06-08/venv/Test/test.py
@@ -1,126 +1,44 @@
-import sys
-import tkinter
-import tkinter.messagebox
-from tkintermapview import TkinterMapView
+import tkinter as tk
+from tkinter import ttk
 
 
-class App(tkinter.Tk):
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-    APP_NAME = "map_view_demo.py"
-    WIDTH = 800
-    HEIGHT = 750
+        # root window
+        self.title('Theme Demo')
+        self.geometry('400x300')
+        self.style = ttk.Style(self)
 
-    def __init__(self, *args, **kwargs):
-        tkinter.Tk.__init__(self, *args, **kwargs)
+        # label
+        label = ttk.Label(self, text='Name:')
+        label.grid(column=0, row=0, padx=10, pady=10,  sticky='w')
+        # entry
+        textbox = ttk.Entry(self)
+        textbox.grid(column=1, row=0, padx=10, pady=10,  sticky='w')
+        # button
+        btn = ttk.Button(self, text='Show')
+        btn.grid(column=2, row=0, padx=10, pady=10,  sticky='w')
 
-        self.title(self.APP_NAME)
-        self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+        # radio button
+        self.selected_theme = tk.StringVar()
+        theme_frame = ttk.LabelFrame(self, text='Themes')
+        theme_frame.grid(padx=10, pady=10, ipadx=20, ipady=20, sticky='w')
 
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.bind("<Return>", self.search)
+        for theme_name in self.style.theme_names():
+            rb = ttk.Radiobutton(
+                theme_frame,
+                text=theme_name,
+                value=theme_name,
+                variable=self.selected_theme,
+                command=self.change_theme)
+            rb.pack(expand=True, fill='both')
 
-        if sys.platform == "darwin":
-            self.bind("<Command-q>", self.on_closing)
-            self.bind("<Command-w>", self.on_closing)
-
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=0)
-        self.grid_columnconfigure(2, weight=0)
-        self.grid_rowconfigure(1, weight=1)
-
-        self.search_bar = tkinter.Entry(self, width=50)
-        self.search_bar.grid(row=0, column=0, pady=10, padx=10, sticky="we")
-        self.search_bar.focus()
-
-        self.search_bar_button = tkinter.Button(master=self, width=8, text="Search", command=self.search)
-        self.search_bar_button.grid(row=0, column=1, pady=10, padx=10)
-
-        self.search_bar_clear = tkinter.Button(master=self, width=8, text="Clear", command=self.clear)
-        self.search_bar_clear.grid(row=0, column=2, pady=10, padx=10)
-
-        self.map_widget = TkinterMapView(width=self.WIDTH, height=600, corner_radius=0)
-        self.map_widget.grid(row=1, column=0, columnspan=3, sticky="nsew")
-
-        self.marker_list_box = tkinter.Listbox(self, height=8)
-        self.marker_list_box.grid(row=2, column=0, columnspan=1, sticky="ew", padx=10, pady=10)
-
-        self.listbox_button_frame = tkinter.Frame(master=self)
-        self.listbox_button_frame.grid(row=2, column=1, sticky="nsew", columnspan=2)
-
-        self.listbox_button_frame.grid_columnconfigure(0, weight=1)
-
-        self.save_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="save current marker",
-                                                 command=self.save_marker)
-        self.save_marker_button.grid(row=0, column=0, pady=10, padx=10)
-
-        self.clear_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="clear marker list",
-                                                  command=self.clear_marker_list)
-        self.clear_marker_button.grid(row=1, column=0, pady=10, padx=10)
-
-        self.connect_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="connect marker with path",
-                                                    command=self.connect_marker)
-        self.connect_marker_button.grid(row=2, column=0, pady=10, padx=10)
-
-        self.map_widget.set_address("NYC")
-
-        self.marker_list = []
-        self.marker_path = None
-
-        self.search_marker = None
-        self.search_in_progress = False
-
-    def search(self, event=None):
-        if not self.search_in_progress:
-            self.search_in_progress = True
-            if self.search_marker not in self.marker_list:
-                self.map_widget.delete(self.search_marker)
-
-            address = self.search_bar.get()
-            self.search_marker = self.map_widget.set_address(address, marker=True)
-            if self.search_marker is False:
-                # address was invalid (return value is False)
-                self.search_marker = None
-            self.search_in_progress = False
-
-    def save_marker(self):
-        if self.search_marker is not None:
-            self.marker_list_box.insert(tkinter.END, f" {len(self.marker_list)}. {self.search_marker.text} ")
-            self.marker_list_box.see(tkinter.END)
-            self.marker_list.append(self.search_marker)
-
-    def clear_marker_list(self):
-        for marker in self.marker_list:
-            self.map_widget.delete(marker)
-
-        self.marker_list_box.delete(0, tkinter.END)
-        self.marker_list.clear()
-        self.connect_marker()
-
-    def connect_marker(self):
-        print(self.marker_list)
-        position_list = []
-
-        for marker in self.marker_list:
-            position_list.append(marker.position)
-
-        if self.marker_path is not None:
-            self.map_widget.delete(self.marker_path)
-
-        if len(position_list) > 0:
-            self.marker_path = self.map_widget.set_path(position_list)
-
-    def clear(self):
-        self.search_bar.delete(0, last=tkinter.END)
-        self.map_widget.delete(self.search_marker)
-
-    def on_closing(self, event=0):
-        self.destroy()
-        exit()
-
-    def start(self):
-        self.mainloop()
+    def change_theme(self):
+        self.style.theme_use(self.selected_theme.get())
 
 
 if __name__ == "__main__":
     app = App()
-    app.start()
+    app.mainloop()
