5c2ac524cc098ab6a710616aab06e3030081931f
Satoshi <denki.satoshi@gmail.com>
2024-07-11 22:59:37 +0200

    Projet To Do List avec PySide6
    Codes opérationnels:
    - Création d'une fenêtre
    - Apparition des widgets
    - Texte en arrière plan sur le LineEdit
    - Bouton Clear => OK
    - Appuie sur "Entrée" => OK

---
+++ __5__Docstrings/__90__Projets/__8__To_do_list_GUI/venv/main.py
@@ -0,0 +1,61 @@
+# Projet: To Do List
+# But:
+# Créer une interface pour ajouter/supprimer une tâche à faire (voir __init__ pour plus d'infos)
+# ----------------------------------------------------------------------------------------------
+# Date de création: 2024-07-09
+# Date de modification: 2024-07-11
+###################################
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QListWidget
+
+
+class MainWindow(QWidget): #La classe hérite de QWidget
+    def __init__(self, nom):
+        super().__init__()
+        self.nom = nom
+        self.setWindowTitle(nom)
+        self.resize(250,350)
+        self.main_layout = QVBoxLayout(self)
+
+        self.lw_todoList = QListWidget()
+        self.le_text = QLineEdit(self)
+        self.le_text.setPlaceholderText("Tâche à effectuer...") # Permet de mettre un texte en arrière plan
+        self.btn_clear = QPushButton("Clear list")
+
+        self.main_layout.addWidget(self.lw_todoList)
+        self.main_layout.addWidget(self.le_text)
+        self.main_layout.addWidget(self.btn_clear)
+
+        self.le_text.textChanged.connect(self.le_text.setText)
+        self.le_text.returnPressed.connect(self.add_text_list) # Ajouter la tâche dans la liste en appuyant sur Entrée
+        self.btn_clear.clicked.connect(self.clear_text_list) # Efface toute la liste ainsi que la saisie
+
+    def add_text_list(self):
+        """
+        Si le champ n'est pas vide, on enregistre le texte du champ dans la liste
+
+        :return:
+        - Ajoute un élément à la liste
+        - Supprime les données du champ
+        """
+        if self.le_text.text() != "":
+            self.lw_todoList.addItem(self.le_text.text())
+        self.le_text.clear()
+
+    def clear_text_list(self):
+        """
+        Supprime toutes les données utilisateurs
+        :return:
+        - N/A
+        """
+        self.le_text.clear()
+        self.lw_todoList.clear()
+
+    def enter_touch(self):
+        print(self.le_text.returnPressed)
+
+
+app = QApplication() #1 seule  QApplication
+main_window = MainWindow("To Do List")
+main_window.show()
+
+app.exec() #Lancer l'application
