11c8724c6adc0c40a343b1e2ef546fcbb77ef59b
Satoshi <denki.satoshi@gmail.com>
2024-07-14 22:48:10 +0200

    Appoint V2 finished
    - Ajout de message pour stocker le message à afficher (V2)
    - La valeur nombre est arrondi à 2 chiffres (V2)
    - Mise en forme du message à afficher (V2)

--- __5__Docstrings/__91__Challenge/2024-07-13/venv/main.py
+++ __5__Docstrings/__91__Challenge/2024-07-13/venv/main.py
@@ -3,10 +3,12 @@
 # Contient la logique du programme
 # -----------------------------------
 # Date de création: 2024-07-13
-# Date de dernière modification: 2024-07-13
+# Date de dernière modification: 2024-07-14
 # ----------------------------------------------------------------
-# version: 1.0
-# -
+# version: 2.0
+# - Ajout de message pour stocker le message à afficher (V2)
+# - La valeur nombre est arrondi à 2 chiffres (V2)
+# - Mise en forme du message à afficher (V2)
 #-------------------------------------------------------------------
 
 # Appel des modules externes
@@ -15,18 +17,20 @@
 from constantes import DEVISE_MEXICAINE
 
 
-def decomposition_monnaie(nombre: int):
+def decomposition_monnaie(nombre: int) -> str:
     """
     Décompose le nombre en différents billets
     :param
     nombre (int): Argent à décomposer
     :return:
-    affiche le billet et la quantité
+    message (str): affiche le billet et la quantité
     """
+    message = ""
     for key, value in DEVISE_MEXICAINE.items():
-        if nombre >= key:
-            print(f"{value} : ", int(nombre / key))
+        if round(nombre,2) >= key:
+            message += f"{value} : {int(round(nombre,2) / key)}//"
             nombre -= key * int(nombre / key)
+    return message
 
 if __name__ == '__main__':
     try:
@@ -34,4 +38,5 @@ if __name__ == '__main__':
     except ValueError:
         print("Erreur de type. Inscrire un nombre!!")
     else:
-        decomposition_monnaie(nombre)
+        print(decomposition_monnaie(nombre).replace("//","\n"))
+
