c5bc1d63b32e20f0e12d1d2a76ede58a20db77f3
Satoshi <denki.satoshi@gmail.com>
2024-09-16 22:15:00 +0200

    Release V1 Game mystere

--- __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/constantes.py
+++ __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/constantes.py
@@ -2,10 +2,14 @@
 # But:
 # Contient les constantes du code
 # -----------------------------------
-# Date de création:
-# Date de dernière modification:
+# Date de création: 2024-09
+# Date de dernière modification: 2024-09-14
 # ------------------------------------------
 # version: 1.0
 # -
 #-------------------------------------------
 
+YELLOW = "\033[1;33m"
+PURPLE = "\033[0;35m"
+GREEN = "\033[0;32m"
+WHITE = "\033[0m"
--- __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/main.py
+++ __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/main.py
@@ -3,7 +3,7 @@
 # Contient la logique du code
 # -----------------------------------
 # Date de création: 2024-09-13
-# Date de dernière modification: 2024-09-13
+# Date de dernière modification: 2024-09-14
 # ------------------------------------------
 # version: 1.0
 # -
@@ -12,12 +12,17 @@
 # Appel des modules externes
 import random
 
+# Appel des modules internes
+from constantes import YELLOW, GREEN, PURPLE, WHITE
 
 # Variables globales
 NOMBRE_MYSTERE = random.randint(1,100)
 
 
 class Game_Mystere():
+    """
+    Classe contenant les méthodes pour la logique du jeu
+    """
 
     def __init__(self, nombre_essais, essais_restants):
         self.nombres_essais = nombre_essais
@@ -25,16 +30,41 @@ class Game_Mystere():
         self.NOMBRE_MYSTERE = NOMBRE_MYSTERE
 
     def bigger(self):
+        """
+        Indique au joueur que le nombre mystère est plus grand
+        :return:
+        (int): Nombre d'essais restants
+        """
         print(f"\nLe nombre mystère est plus grand\n")
         return self.essais_restants - 1
 
     def smaller(self):
+        """
+        Indique au joueur que le nombre mystère est plus petit
+        :return:
+        (int): Nombre d'essais restants
+        """
         print(f"\nLe nombre mystère est plus petit\n")
         return self.essais_restants - 1
 
     def user_try(self):
+        """
+        Indique au joueur que le nombre d'essais restants, et demande un nouvel essai
+        :return:
+        (int): Nombre inscrit par le joueur
+        """
         print(f"\nEssai possible: {essais_restants}/{nombre_essais}")
-        return int(input("Veuillez entre un nombre entre 1 et 100: "))
+        if self.essais_restants == 1:
+            print(f"{PURPLE}DERNIER ESSAI:")
+        return int(input(f"Veuillez entre un nombre entre 1 et 100: {WHITE}"))
+
+    def error_input(self):
+        """
+        Indique au joueur que son nombre n'est pas compris entre 1 et 100
+        :return:
+        """
+        print(f"{YELLOW}ERREUR!! Veuillez écrire un nombre entre 1 et 100....{WHITE}")
+
 
 
 if __name__ == "__main__":
@@ -48,9 +78,15 @@ if __name__ == "__main__":
         jeu_mystere = Game_Mystere(nombre_essais, essais_restants)
         nombre_utilisateur = jeu_mystere.user_try()
 
+        if nombre_utilisateur < 1 or nombre_utilisateur > 100 :
+            jeu_mystere.error_input()
+            continue
+
         if NOMBRE_MYSTERE > nombre_utilisateur:
             essais_restants = jeu_mystere.bigger()
         elif NOMBRE_MYSTERE < nombre_utilisateur:
             essais_restants = jeu_mystere.smaller()
+        elif NOMBRE_MYSTERE == nombre_utilisateur:
+            break
 
-    print("Fin du jeu")
+    print(f"{GREEN}Bravo, vous avez trouvé le nombre mystère!!{WHITE}")
