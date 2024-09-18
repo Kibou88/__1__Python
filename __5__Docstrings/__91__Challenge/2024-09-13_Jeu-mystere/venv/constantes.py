91fe3b53062b1156df06e5231385668bec7dee6e
Satoshi <denki.satoshi@gmail.com>
2024-09-18 22:56:18 +0200

    Jeu_mystère v3:
    - Appel de la classe Colors pour le changement de couleurs du texte (V3)
    - Suppresion des méthodes "intro_number_try" et "error_input"(V3)
    - Ajout opérateur ternaire dans "user_try" (V3)
    - Message d'accueil dans le constructeur "__init__" (V3)
    - Simplification de la méthode "difference_user_mystere" (V3)

--- __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/constantes.py
+++ __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/constantes.py
@@ -5,12 +5,14 @@
 # Date de création: 2024-09
 # Date de dernière modification: 2024-09-15
 # ------------------------------------------
-# version: 2.0
-# - Ajout de la couleur rouge
+# version: 3.0
+# - Ajout de la couleur rouge (V2)
+# - Ajout de la classe couleur contenant les couleurs utilisés (V3)
 #-------------------------------------------
 
-YELLOW = "\033[1;33m"
-PURPLE = "\033[0;35m"
-GREEN = "\033[0;32m"
-WHITE = "\033[0m"
-RED = "\033[0;31m"
+class Colors:
+    YELLOW = "\033[1;33m"
+    PURPLE = "\033[0;35m"
+    GREEN = "\033[0;32m"
+    WHITE = "\033[0m"
+    RED = "\033[0;31m"
--- __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/main.py
+++ __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/main.py
@@ -3,22 +3,27 @@
 # Contient la logique du code
 # -----------------------------------
 # Date de création: 2024-09-13
-# Date de dernière modification: 2024-09-16
+# Date de dernière modification: 2024-09-18
 # ------------------------------------------
-# version: 2.0
-# - Création des méthodes "intro_number_try", "final_message", "difference_user_mystere"
-# - Amélioration d'une mauvaise saisie utilisateur
-# - Simplification du code avec l'ajout des nouvelles méthodes
+# version: 3.0
+# - Création des méthodes "intro_number_try", "final_message", "difference_user_mystere" (V2)
+# - Amélioration d'une mauvaise saisie utilisateur (V2)
+# - Simplification du code avec l'ajout des nouvelles méthodes (V2)
+# - Appel de la classe Colors pour le changement de couleurs du texte (V3)
+# - Suppresion des méthodes "intro_number_try" et "error_input"(V3)
+# - Ajout opérateur ternaire dans "user_try" (V3)
+# - Message d'accueil dans le constructeur "__init__" (V3)
+# - Simplification de la méthode "difference_user_mystere" (V3)
 #-------------------------------------------
 
 # Appel des modules externes
 import random
 
 # Appel des modules internes
-from constantes import YELLOW, GREEN, PURPLE, WHITE, RED
+from constantes import Colors
 
 
-class Game_Mystere():
+class Game_Mystere:
     """
     Classe contenant les méthodes pour la logique du jeu
     """
@@ -29,45 +34,31 @@ class Game_Mystere():
         self.nombre_essais = 0
         self.nombre_mystere = random.randint(1, 100)
 
-    def intro_number_try(self):
-        """
-        Affiche le message du début +  nombre d'essais choisis par l'utilisateur
-        :return:
-        """
         print("Bienvenue dans le jeu Le Nombre Mystère\n")
         self.nombre_essais = int(input("Veuillez choisir le nombre d'essai maximum: "))
         self.essais_restants = self.nombre_essais
 
-    def bigger(self):
+    def user_try(self):
         """
-        Indique au joueur que le nombre mystère est plus grand
+        Indique au joueur que le nombre d'essais restants, et vérifie la saisie utilisateur
         :return:
-        (int): Nombre d'essais restants
+        (int): Nombre inscrit par le joueur
         """
-        print(f"\nLe nombre mystère est plus grand\n")
-        self.essais_restants -= 1
+        print(f"\nEssai possible: {self.essais_restants}/{self.nombre_essais}")
 
-    def smaller(self):
-        """
-        Indique au joueur que le nombre mystère est plus petit
-        :return:
-        (int): Nombre d'essais restants
-        """
-        print(f"\nLe nombre mystère est plus petit\n")
-        self.essais_restants -= 1
+        print(f'{Colors.PURPLE}DERNIER ESSAI:' if self.essais_restants == 1 else '')
 
-    def final_message(self):
-        """
-        Message si l'utilisateur a trouvé le nombre ou si il a perdu
-        :return:
-        (bool): Renvoie la valeur True pour quitter le jeu
-        """
-        if self.essais_restants == 0:
-            print(f"{RED}PERDU, le nombre mystère était {self.nombre_mystere}!!{WHITE}")
-        elif self.nombre_mystere == self.nombre_utilisateur:
-            print(f"{GREEN}Bravo, vous avez trouvé le nombre mystère!!{WHITE}")
+        try:
+            self.nombre_utilisateur = int(input(f"Veuillez entre un nombre entre 1 et 100: {Colors.WHITE}"))
+            if self.nombre_utilisateur < 1 or self.nombre_utilisateur > 100:
+                raise IndexError
 
-        return True
+        except ValueError:
+            print(f"{Colors.RED}Veuillez saisir un nombre!!{Colors.WHITE}")
+            return True
+        except IndexError:
+            print(f"{Colors.YELLOW}ERREUR!! Veuillez écrire un nombre entre 1 et 100....{Colors.WHITE}")
+            return True
 
     def difference_user_mystere(self):
         """
@@ -75,53 +66,33 @@ class Game_Mystere():
         :return:
         Affiche le message approprié pour aiguiller l'utilisateur
         """
-        if self.nombre_mystere > self.nombre_utilisateur:
-            return jeu_mystere.bigger()
-        elif self.nombre_mystere < self.nombre_utilisateur:
-            return jeu_mystere.smaller()
+
+        print(f"\nLe nombre mystère est plus grand\n" if self.nombre_mystere > self.nombre_utilisateur else '')
+        print(f"\nLe nombre mystère est plus petit\n" if self.nombre_mystere < self.nombre_utilisateur else '')
+        self.essais_restants -= 1
+
+    def __str__(self):
+
+        return f'{Colors.GREEN}Bravo, vous avez trouvé le nombre mystère!!{Colors.WHITE}'\
+        if self.nombre_mystere == self.nombre_utilisateur else\
+            f'{Colors.RED}PERDU, le nombre mystère était {self.nombre_mystere}!!{Colors.WHITE}'
 
 
-    def user_try(self):
-        """
-        Indique au joueur que le nombre d'essais restants, et demande un nouvel essai
-        :return:
-        (int): Nombre inscrit par le joueur
-        """
-        print(f"\nEssai possible: {self.essais_restants}/{self.nombre_essais}")
-        if self.essais_restants == 1:
-            print(f"{PURPLE}DERNIER ESSAI:")
-        return int(input(f"Veuillez entre un nombre entre 1 et 100: {WHITE}"))
+    def game(self):
 
-    def error_input(self):
-        """
-        Indique au joueur que son nombre n'est pas compris entre 1 et 100 ou que ce n'est pas un nombre
-        :return:
-        """
-        if self.nombre_utilisateur < 1 or self.nombre_utilisateur > 100:
-            print(f"{YELLOW}ERREUR!! Veuillez écrire un nombre entre 1 et 100....{WHITE}")
-            return True
+        while jeu_mystere.essais_restants > 0 and jeu_mystere.nombre_utilisateur != jeu_mystere.nombre_mystere:
+
+            if jeu_mystere.user_try():
+                continue
+
+            jeu_mystere.difference_user_mystere()
+
+            if jeu_mystere.essais_restants == 0 or jeu_mystere.nombre_mystere == jeu_mystere.nombre_utilisateur:
+                print(str(jeu_mystere))
+                break
+
 
 
 if __name__ == "__main__":
     jeu_mystere = Game_Mystere()
-    jeu_mystere.intro_number_try()
-
-    while jeu_mystere.essais_restants > 0 and jeu_mystere.nombre_utilisateur != jeu_mystere.nombre_mystere:
-
-        # Message d'erreur si l'utilisateur n'écrit pas des nombres
-        try:
-            jeu_mystere.nombre_utilisateur = jeu_mystere.user_try()
-        except ValueError:
-            print(f"{RED}Veuillez saisir un nombre!!{WHITE}")
-            continue
-
-        if jeu_mystere.error_input(): # Erreur de saisie. La boucle s'arrête et l'utilisateur ressaisi un nouveau nombre
-            continue
-
-        jeu_mystere.difference_user_mystere()
-
-        if jeu_mystere.final_message():
-            break
-
-
-
+    jeu_mystere.game()
