f1681484ae4936bcb403e2a4328210c2f0f5b9f6
Satoshi <denki.satoshi@gmail.com>
2024-09-27 22:09:42 +0200

    Jeu Mystere V5:
    - 1 ligne pour appeler la classe (V5)
    - Création d'une méthode privée __start + passage en méthode statique (V5)
    - NOMBRE_MYSTERE passe en attribut de classe (V5)
    - Simplifications de toutes les méthodes de la classe (V5)
    - Structure du jeu (méthode game) plus efficace (V5)

--- __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/main.py
+++ __5__Docstrings/__91__Challenge/2024-09-13_Jeu-mystere/venv/main.py
@@ -3,9 +3,9 @@
 # Contient la logique du code
 # -----------------------------------
 # Date de création: 2024-09-13
-# Date de dernière modification: 2024-09-18
+# Date de dernière modification: 2024-09-27
 # ------------------------------------------
-# version: 3.0
+# version: 4.0
 # - Création des méthodes "intro_number_try", "final_message", "difference_user_mystere" (V2)
 # - Amélioration d'une mauvaise saisie utilisateur (V2)
 # - Simplification du code avec l'ajout des nouvelles méthodes (V2)
@@ -16,6 +16,11 @@
 # - Simplification de la méthode "difference_user_mystere" (V3)
 # - Modification "Game_Mystere" by "GameMystere" (V4)
 # - Passage des méthodes "user_try" et "difference_user_mystere" en privée (V4)
+# - 1 ligne pour appeler la classe (V5)
+# - Création d'une méthode privée __start + passage en méthode statique (V5)
+# - NOMBRE_MYSTERE passe en attribut de classe (V5)
+# - Simplifications de toutes les méthodes de la classe (V5)
+# - Structure du jeu (méthode game) plus efficace (V5)
 #-----------------------------------------------------------------------------------
 
 # Appel des modules externes
@@ -29,16 +34,28 @@ class GameMystere:
     """
     Classe contenant les méthodes pour la logique du jeu
     """
+    NOMBRE_MYSTERE = random.randint(1, 100)
 
     def __init__(self):
-        self.nombre_utilisateur = 0
-        self.essais_restants = 0
-        self.nombre_essais = 0
-        self.nombre_mystere = random.randint(1, 100)
-
         print("Bienvenue dans le jeu Le Nombre Mystère\n")
-        self.nombre_essais = int(input("Veuillez choisir le nombre d'essai maximum: "))
+        self. nombre_essais = self.__start()
         self.essais_restants = self.nombre_essais
+        self.nombre_utilisateur = 0
+
+
+    def __str__(self):
+
+        return self.game()
+
+
+    @staticmethod
+    def __start():
+        """
+        Affiche le message de bienvenue + demande au joueur de saisir le nombre d'essais
+        :return:
+        (int): nombre essais et nombre essais restants
+        """
+        return int(input("Veuillez choisir le nombre d'essai maximum: "))
 
     def __user_try(self):
         """
@@ -46,21 +63,14 @@ class GameMystere:
         :return:
         (int): Nombre inscrit par le joueur
         """
-        print(f"\nEssai possible: {self.essais_restants}/{self.nombre_essais}")
+        print(f'{Colors.PURPLE}DERNIER ESSAI:' if self.essais_restants == 1 else
+              f"\nEssai possible: {self.essais_restants}/{self.nombre_essais}")
 
-        print(f'{Colors.PURPLE}DERNIER ESSAI:' if self.essais_restants == 1 else '')
-
-        try:
-            self.nombre_utilisateur = int(input(f"Veuillez entre un nombre entre 1 et 100: {Colors.WHITE}"))
-            if self.nombre_utilisateur < 1 or self.nombre_utilisateur > 100:
-                raise IndexError
-
-        except ValueError:
-            print(f"{Colors.RED}Veuillez saisir un nombre!!{Colors.WHITE}")
-            return True
-        except IndexError:
+        while True:
+            choix_utilisateur = input(f"Veuillez entre un nombre entre 1 et 100: {Colors.WHITE}")
+            if choix_utilisateur.isdigit() and 1 <= int(choix_utilisateur) <= 100:
+                return int(choix_utilisateur)
             print(f"{Colors.YELLOW}ERREUR!! Veuillez écrire un nombre entre 1 et 100....{Colors.WHITE}")
-            return True
 
     def __difference_user_mystere(self):
         """
@@ -68,32 +78,29 @@ class GameMystere:
         :return:
         Affiche le message approprié pour aiguiller l'utilisateur
         """
-
-        print(f"\nLe nombre mystère est plus grand\n" if self.nombre_mystere > self.nombre_utilisateur else '')
-        print(f"\nLe nombre mystère est plus petit\n" if self.nombre_mystere < self.nombre_utilisateur else '')
-        self.essais_restants -= 1
-
-    def __str__(self):
-
-        return f'{Colors.GREEN}Bravo, vous avez trouvé le nombre mystère!!{Colors.WHITE}'\
-        if self.nombre_mystere == self.nombre_utilisateur else\
-            f'{Colors.RED}PERDU, le nombre mystère était {self.nombre_mystere}!!{Colors.WHITE}'
+        return f"\nLe nombre mystère est plus grand\n" if GameMystere.NOMBRE_MYSTERE > self.nombre_utilisateur else \
+            f"\nLe nombre mystère est plus petit\n"
 
 
     def game(self):
+        """
+        Contient la structure du jeu
+        :return:
+        Les phrases si le joueur a gagné ou perdu
+        """
 
-        while self.essais_restants > 0 and self.nombre_utilisateur != self.nombre_mystere:
+        while self.essais_restants > 0 and self.nombre_utilisateur != GameMystere.NOMBRE_MYSTERE:
+            self.nombre_utilisateur = self.__user_try()
+            self.essais_restants -= 1
+            
+            if self.nombre_utilisateur == GameMystere.NOMBRE_MYSTERE:
+                return f'{Colors.GREEN}Bravo, vous avez trouvé le nombre mystère!!{Colors.WHITE}'
+            if self.essais_restants == 0:
+                return f'{Colors.RED}PERDU, le nombre mystère était {GameMystere.NOMBRE_MYSTERE}!!{Colors.WHITE}'
 
-            if jeu_mystere.__user_try():
-                continue
+            print(self.__difference_user_mystere())
 
-            jeu_mystere.__difference_user_mystere()
-
-            if self.essais_restants == 0 or self.nombre_mystere == self.nombre_utilisateur:
-                print(str(jeu_mystere))
-                break
 
 
 if __name__ == "__main__":
-    jeu_mystere = GameMystere()
-    jeu_mystere.game()
+    print(GameMystere())
