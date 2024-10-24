679627f1b9ccc9e25343c5ed5bf570c95b419148
Satoshi <denki.satoshi@gmail.com>
2024-10-24 22:24:41 +0200

    Expedition mars:
    - Rajoute des types hinting

--- __5__Docstrings/__91__Challenge/2024-09-27_Expedition_martienne/venv/main.py
+++ __5__Docstrings/__91__Challenge/2024-09-27_Expedition_martienne/venv/main.py
@@ -3,21 +3,22 @@
 # Contient la logique du code
 # -----------------------------------
 # Date de création: 2024-09-27
-# Date de dernière modification: 2024-10-23
+# Date de dernière modification: 2024-10-24
 # ------------------------------------------
-# version: 2.0
-# - Suppression de l'arrondit à 10^3
-# - Modification "return list_nodes" par "return list_nodes.reverse()"
-# - Fonction "shortest_distance" suppresion des ".keys()" non nécessaire
-# - Fonction "convert_list_dico" simplificatio avec setdefault
-# - Amélioration efficience des messages en 1 print
+# version: 3.0
+# - Suppression de l'arrondit à 10^3 (V2)
+# - Modification "return list_nodes" par "return list_nodes.reverse()" (V2)
+# - Fonction "shortest_distance" suppresion des ".keys()" non nécessaire (V2)
+# - Fonction "convert_list_dico" simplificatio avec setdefault (V2)
+# - Amélioration efficience des messages en 1 print (V2)
+# - Rajoute des types hinting (V3)
 #-----------------------------------------------------------------------------------
 
 # Appel des modules internes
 from ressources_mars import POINTS_DEPART, POINTS_ARRIVEE, DISTANCES_DES_LIAISONS
 
 
-def convert_list_dico(distances_liaisons):
+def convert_list_dico(distances_liaisons: list) -> dict:
     """
     Fonction pour convertir la table en dico
     :param distances_liaisons (lst): Liste de distance entre 2 points
@@ -32,14 +33,14 @@ def convert_list_dico(distances_liaisons):
 
     return dico
 
-def shortest_distance(dico_distance, depart):
+def shortest_distance(dico_distance: dict, depart: str) -> dict:
     """
     Fonction pour déterminer la distance la plus courte pour chaque point
     :param dico_distance (dict): Dictionnaire des différents points à passer
     :param depart (str): Point de départ
     :return:
-    - distance(dict): Dictionnaire pour la distance totale du point de départ pour chaque point
-    - previous_nodes(dict): Dictionnaire affichant le point le plus court (valeur) par rapport au point actuel (clé)
+    - distance (dict): Dictionnaire pour la distance totale du point de départ pour chaque point
+    - previous_nodes (dict): Dictionnaire affichant le point le plus court (valeur) par rapport au point actuel (clé)
     """
     previous_nodes = {node: None for node in dico_distance}
     visited = {x:False for x in dico_distance}
@@ -53,7 +54,7 @@ def shortest_distance(dico_distance, depart):
         node_distance, node = current_node.pop()
         if not visited[node]:
             visited[node] = True
-            for neighbor in dico_distance[node].keys():
+            for neighbor in dico_distance[node]:
                 neighbor_distance = node_distance + dico_distance[node][neighbor]
                 if neighbor_distance < distance[neighbor]:
                     distance[neighbor] = neighbor_distance
@@ -63,14 +64,14 @@ def shortest_distance(dico_distance, depart):
 
     return distance, previous_nodes
 
-def recap_point(previous_node, depart, arrivee):
+def recap_point(previous_node: dict, depart: str, arrivee: str) -> list:
     """
     Fonction pour retrouver les noeuds composant le chemin le plus court
     :param previous_node (dict): dictionnaire contenant tout les noeuds et leur distance
     :param depart (str): Noeud de départ
     :param arrivee (str): Noeud d'arrivée
     :return:
-    - list_nodes (str): Liste des noeuds du point de départ à l'arrivée
+    - list_nodes (list): Liste des noeuds du point de départ à l'arrivée
     """
     list_nodes = [arrivee]
     node = arrivee
