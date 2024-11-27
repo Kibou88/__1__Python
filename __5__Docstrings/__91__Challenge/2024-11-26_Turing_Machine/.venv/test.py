96cd2614e361522a6f6ab354a009e42d7c213c00
Kibou88_Ubuntu <denki.satoshi@gmail.com>
2024-11-27 21:56:57 +0100

    Test with sys.argv

---
+++ __5__Docstrings/__91__Challenge/2024-11-26_Turing_Machine/.venv/test.py
@@ -0,0 +1,7 @@
+import sys
+
+print("0:", sys.argv[0])
+print("1:", sys.argv[1]) #programme
+print("2:", sys.argv[2]) #ruban
+print("3:", sys.argv[3]) #init & final set
+print("4:", sys.argv[4]) #position intial tête de lecture
