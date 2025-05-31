aafb10b1b8ca2c9ab2befe1c3c3aaca2889ca5ad
Satoshi <denki.satoshi@gmail.com>
2025-05-31 22:20:25 +0200

    add comment, wrong user input, delete last character

--- __5__Docstrings/__90__Projets/__12__Calculatrice_GUI/main.py
+++ __5__Docstrings/__90__Projets/__12__Calculatrice_GUI/main.py
@@ -17,7 +17,7 @@ from PySide6 import QtCore
 from PySide6.QtGui import QShortcut, QKeySequence
 from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QPushButton
 
-from boutons import BUTTONS
+from boutons import BUTTONS, OPERATIONS
 
 
 class Calculator(QWidget): #La classe hérite de QWidget
@@ -50,32 +50,75 @@ class Calculator(QWidget): #La classe hérite de QWidget
         self.button["="].clicked.connect(self.resultat_operation)
         self.connect_keyboard_shortcut()
 
+    @property
+    def result(self):
+        """
+        Utiliser cette propriété au lieu d'utiliser 'self.result'
+        """
+        return self.le_result.text()
+
     def clear_result(self):
+        """
+        Permet d'effacer toutes les informations du Line Edit
+        """
         self.le_result.setText("0")
 
     def resultat_operation(self):
+        """
+        Interpètre la chaine de caractère venant du Line Edit en code Python.
+        Lève une exception de type SyntaxError et l'affiche sur le Line Edit
+        Sinon:
+         - Renvoie le calcul en str
+        """
         try:
             #eval va permettre d'analyser la chaine de caractère et l'interpréter en code python
             #si eval lit "5 + 10", il comprends : 5 + 10 => 15
-            result = eval(self.le_result.text().replace("x", "*"))
+            result = eval(self.result.replace("x", "*"))
         except SyntaxError:
             self.le_result.setText("Erreur de syntaxe")
 
         else:
             self.le_result.setText(str(result))
     def number_or_operations_pressed(self):
-        if self.le_result.text() == "0" or self.le_result.text() == "Erreur de syntaxe":
+        """
+        1er bloc if:
+            Permet d'éviter des mauvaises erreures de saisies
+
+        2nd bloc if:
+            Permet d'afficher la saisie utilisateur en réinitialisation l'affiche si présence du '0'
+            ou 'Erreur de syntaxe'
+        """
+        if self.sender().text() in OPERATIONS:
+            if self.result[-1] in OPERATIONS or self.result == '0':
+                return
+
+
+        if self.result == "0" or self.result == "Erreur de syntaxe":
             self.le_result.clear()
         # print(self.sender().text())
-        self.le_result.setText(self.le_result.text() + self.sender().text())
+        self.le_result.setText(self.result + self.sender().text())
 
     def connect_keyboard_shortcut(self):
+        """
+        Gère la gestion des appuis claviers et les connectent aux fonctions adéquates
+        """
         for button_text, button in self.button.items():
             QShortcut(QKeySequence(button_text), self, button.clicked.emit)
+    # QShortcut(QKeySequence(touche clavier), widget sur lequel l'évènement est déclenché, méthode qui est déclenché
+    #                                            self: fenêtre actuelle
 
         QShortcut(QKeySequence(QtCore.Qt.Key_Space), self, self.resultat_operation)
         QShortcut(QKeySequence(QtCore.Qt.Key_Return), self, self.resultat_operation)
+        QShortcut(QKeySequence(QtCore.Qt.Key_Backspace), self, self.delete_last_character)
 
+    def delete_last_character(self):
+        """
+        Permet de supprimer le dernier caractère ou d'afficher '0' si tous les caractères ont été éffacés
+        """
+        if len(self.result) > 1:
+            self.le_result.setText(self.result[:-1])
+        else:
+            self.le_result.setText("0")
 
 
 # -----------------------------------------------------------------------
