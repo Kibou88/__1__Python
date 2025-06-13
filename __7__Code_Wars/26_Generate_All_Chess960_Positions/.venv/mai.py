0cd480663f8df231c1bab6626bfce1c617ad4808
Satoshi <denki.satoshi@gmail.com>
2025-06-13 14:47:11 +0200

    exo 26 forfait à cause du timeout

--- __7__Code_Wars/26_Generate_All_Chess960_Positions/.venv/mai.py
+++ __7__Code_Wars/26_Generate_All_Chess960_Positions/.venv/mai.py
@@ -1,10 +1,14 @@
 import itertools
 
+import itertools
+
+
 def get_chess960_position(n):
     chess960_positions = []
     pieces = ['b', 'b', 'n', 'n', 'r', 'r', 'k', 'q']
     pawn = ["p"] * 8
     empty_case = ["."] * 8
+    temp = []
 
     for perm_piece in itertools.permutations(pieces):
         index_r = [i for i, piece in enumerate(perm_piece) if piece == 'r']
@@ -15,30 +19,35 @@ def get_chess960_position(n):
             continue
         if len(index_b) != 2 or index_b[0] % 2 == index_b[1] % 2:
             continue
-
-        chess_table = []
-        for row in range(8):
-            if row == 0:
-                chess_table.append(" ".join(perm_piece))
-            elif row == 1:
-                chess_table.append(" ".join(pawn))
-            elif 2 <= row <= 5:
-                chess_table.append(" ".join(empty_case))
-            elif row == 6:
-                chess_table.append(" ".join([p.upper() for p in pawn]))
-            elif row == 7:
-                chess_table.append(" ".join([p.upper() for p in perm_piece]))
-        chess960_positions.append("\n".join(chess_table))
-
-    chess960_positions = list(set(chess960_positions))
+        temp.append(perm_piece)
+        set_chess = set(temp)
+    chess960_positions = list(set_chess)
     chess960_positions.sort()
 
-    return chess960_positions[n-1]
+    chess_table = []
+    for row in range(8):
+        if row == 0:
+            chess_table.append(" ".join(chess960_positions[n - 1]))
+        elif row == 1:
+            chess_table.append(" ".join(pawn))
+        elif 2 <= row <= 5:
+            chess_table.append(" ".join(empty_case))
+        elif row == 6:
+            chess_table.append(" ".join([p.upper() for p in pawn]))
+        elif row == 7:
+            chess_table.append(" ".join([p.upper() for p in chess960_positions[n - 1]]))
+    #     print("\\n".join(chess_table)+"\\n")
+
+    return "\\n".join(chess_table) + "\\n"
 
 
 
 if __name__ == '__main__':
+    print("n=1")
     get_chess960_position(1)
+    print("n=480")
     get_chess960_position(480)
+    print("n=829")
     get_chess960_position(829)
+    print("n=960")
     get_chess960_position(960)
--- __7__Code_Wars/26_Generate_All_Chess960_Positions/.venv/test.py
+++ __7__Code_Wars/26_Generate_All_Chess960_Positions/.venv/test.py
@@ -1,9 +1,40 @@
 import itertools
 chess960_positions = []
+temp = []
 chess_table = ""
 pawn = ["p", "p", "p", "p", "p", "p", "p", "p"]
 empty_case = [".", ".", ".", ".", ".", ".", ".", "."]
 pieces = ['b', 'b', 'n', 'n', 'q', 'r', 'k', 'r']
 
-pieces.sort()
-print(pieces)
+for perm_piece in itertools.permutations(pieces):
+    index_r = [i for i, piece in enumerate(perm_piece) if piece == 'r']
+    index_k = perm_piece.index('k')
+    index_b = [j for j, piece in enumerate(perm_piece) if piece == 'b']
+
+    if not (min(index_r) < index_k < max(index_r)):
+        continue
+    if len(index_b) != 2 or index_b[0] % 2 == index_b[1] % 2:
+        continue
+    temp.append(perm_piece)
+
+# print(len(set(chess960_positions))) 960 positions en enlevant les doublons
+set_chess = set(temp)
+chess960_positions = list(set_chess)
+# print(len(chess960_positions)) 960 positions
+print(chess960_positions[-1])
+chess960_positions.sort()
+print(chess960_positions[0])
+
+chess_table = []
+for row in range(8):
+    if row == 0:
+        chess_table.append(" ".join(chess960_positions[-1]))
+    elif row == 1:
+        chess_table.append(" ".join(pawn))
+    elif 2 <= row <= 5:
+        chess_table.append(" ".join(empty_case))
+    elif row == 6:
+        chess_table.append(" ".join(pawn).upper())
+    elif row == 7:
+        chess_table.append(" ".join(chess960_positions[-1]).upper())
+print("\\n".join(chess_table)+"\\n")
