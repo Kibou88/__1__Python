f81b46b113af32911db1adb7832a14e757cd8d0b
Satoshi <denki.satoshi@gmail.com>
2025-07-07 09:43:11 +0200

    encode-decode-XOR:
    Version V1.1:
    - Partie "début du message connu" ajoutée (V1.1)
    - Ajout de couleur (V1.1)
    - Ajustement de la méthode "decode_message" (V1.1)

--- __8__TryHackMe/1_W1seGuy/encode-decode-XOR.py
+++ __8__TryHackMe/1_W1seGuy/encode-decode-XOR.py
@@ -6,10 +6,15 @@
 # Date de création: 2025-06-27
 # Date de modification: 2025-06-27
 # --------------------------------------------------------------------------------
-# Version V1
+# Version V1.1:
+# - Partie "début du message connu" ajoutée (V1.1)
+# - Ajout de couleur (V1.1)
+# - Ajustement de la méthode "decode_message" (V1.1)
+
 import binascii
 from string import digits, ascii_letters
 
+from class_colors import *
 
 class Encode_Message():
     def __init__(self, _message):
@@ -25,7 +30,7 @@ class Decode_Message():
     """
     Permet de décoder un message XOR
     """
-    def __init__(self, _message, _length_key=0, _key=None):
+    def __init__(self, _message, _length_key=None, _key=None):
         self._message = _message
         self.encrypted_bytes = binascii.unhexlify(_message) # Conversion du message hexa en binaire
         self._length_key = _length_key
@@ -48,45 +53,63 @@ class Decode_Message():
         """
         Permet de trouver la clé de cryptage
         :return:
+        self._key (str): clé trouvée pour le message codée
         """
-        self.HMI()
-        if (self.know_start != "" and self.know_end != ""):
+        # self.HMI_hexa_message()
+        self.HMI_key()
+        if (self.know_start != ""):
+            key = []
+            for index_bin in range(len(self.encrypted_bytes)):
+                if index_bin >= len(self.know_start):
+                    break
+                for ascii_alphanum in ascii_letters:
+                    if (chr(self.encrypted_bytes[index_bin] ^ ord(ascii_alphanum)) == self.know_start[index_bin]):
+                        key.append(ascii_alphanum)
+            self._key = "".join(key)
+
+        elif (self.know_start != "" and self.know_end != ""):
             num_iterations_start = min(len(self.encrypted_bytes), len(self.know_start))
             self.key_start = "".join([chr(self.encrypted_bytes[i]^ord(self.know_start[i])) \
                                       for i in range(num_iterations_start)])
             num_iterations_end = min(len(self.encrypted_bytes), len(self.know_end))
             self.key_end = "".join([chr(self.encrypted_bytes[-num_iterations_end+i] ^ ord(end_test[i])) \
                                     for i in range(num_iterations_end)])
+            self._key = self.key_start + self.key_end
 
         else:
             pass
 
     def decode_message(self):
         """
-        Permet de décoder un message XOR avec la clé trouvée
+        Permet de décoder et d'afficher le message XOR avec la clé trouvée
         :return:
         """
-
-        if (self.key_start != "" and self.key_end != ""):
-            self._key = self.key_start + self.key_end
-            decoded = "".join([chr(self.encrypted_bytes[i] ^ ord(self._key[i % len(self._key)]))\
-                               for i in range(len(self.encrypted_bytes))])
+        decoded = "".join([chr(self.encrypted_bytes[i] ^ ord(self._key[i % len(self._key)])) \
+                           for i in range(len(self.encrypted_bytes))])
+        if (self.know_start != ""):
+            if decoded.startswith(self.know_start):
+                print(f"La cle est: {Colors.YELLOW}{self._key}{Colors.END} et le message est: "
+                      f"{Colors.YELLOW}{decoded}{Colors.END}")
+        elif (self.key_start != "" and self.key_end != ""):
             if decoded.startswith(self.know_start) and decoded.endswith(self.know_end):
-                print(f"La cle est: {self._key} et le message: {decoded}")
-            else:
-                print(f"Il y a eu une erreur")
+                print(f"La cle est: {Colors.YELLOW}{self._key}{Colors.END} et le message est: "
+                      f"{Colors.YELLOW}{decoded}{Colors.END}")
+        else:
+            print(f"Il y a eu une erreur")
 
-    def HMI(self):
+    def HMI_key(self):
         """
         Affiche et permet de faire des choix à l'utilisateur
         :return:
+        self.know_start and/or self.know_end (str): Début et/ou fin du message décodé connu
         """
         if (self._key == None):
             print("Si vous connaissez une partie du message decrypte, pour:")
-            print("\tLe debut, ecrire: start\n"
-                  "\tLa fin, ecrire: end\n"
-                  "\tLes deux, ecrire: both\n"
-                  "\tSinon, ecrire: nothing\n")
+            print(f"\tLe debut, ecrire: {Colors.BLUE}{Colors.BOLD}start{Colors.END}\n"
+                  f"\tLa fin, ecrire: {Colors.BLUE}{Colors.BOLD}end{Colors.END}\n"
+                  f"\tLes deux, ecrire: {Colors.BLUE}{Colors.BOLD}both{Colors.END}\n"
+                  f"\tSinon, ecrire: {Colors.BLUE}{Colors.BOLD}nothing{Colors.END}\n")
+
             key_parts_know = input("Quelle(s) partie(s) du message decrypte connaissez vous? ")
             if (key_parts_know.lower() == "start"):
                 self.know_start = input("Ecrire le debut du message decrypte: ")
@@ -105,10 +128,10 @@ if __name__ == '__main__':
     test = "THM{"
     end_test = "}"
     encoded_message = "381c2401205d35051424292c1d3b2418600a11332d3a1b493100181012051e20104a251e2c26082d"
-    # encrypted_bytes = binascii.unhexlify(encoded_message)
-    # num_iteration = min(len(encrypted_bytes), len(end_test))
-    # print(encrypted_bytes[-2:])
-    # print(chr(encrypted_bytes[-1] ^ ord(end_test[0])))
-    test_to_decode = Decode_Message(encoded_message)
+    XOR_message = ("1c1c01041963730f31352a3a386e24356b3d32392b6f6b0d323c22243f6373"
+                   "1a0d0c302d3b2b1a292a3a38282c2f222d2a112d282c31202d2d2e24352e60")
+
+
+    test_to_decode = Decode_Message(XOR_message)
     test_to_decode.decode_key()
     test_to_decode.decode_message()
