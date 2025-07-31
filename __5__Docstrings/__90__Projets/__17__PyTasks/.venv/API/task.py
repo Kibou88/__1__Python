30853eaf8c41ac3fe0da3a6e9100f65c5740d084
Satoshi <denki.satoshi@gmail.com>
2025-07-31 11:07:47 +0200

    PyTasks completed

--- __5__Docstrings/__90__Projets/__17__PyTasks/.venv/API/task.py
+++ __5__Docstrings/__90__Projets/__17__PyTasks/.venv/API/task.py
@@ -16,7 +16,59 @@ from misc.logs import Logs
 
 current_dir = os.path.dirname(os.path.dirname(__file__))
 
-api_logs = Logs(application_name="Tasks_API", log_dir=f"{current_dir}\Logs")
+api_logs = Logs(application_name="Tasks_API", log_dir=f"{current_dir}\\Logs")
 
 TASK_DIR = os.path.join(current_dir, ".todo")
 TASK_FILEPATH = os.path.join(TASK_DIR, "task.json")
+
+def get_tasks():
+    if os.path.exists(TASK_FILEPATH):
+        with open(TASK_FILEPATH, "r") as f:
+            return json.load(f)
+    return {}
+
+def add_tasks(name):
+    tasks = get_tasks()
+    if name in tasks.keys():
+       api_logs.log_error(f"{name} already added")
+       return False
+
+    tasks[name] = False
+    _write_tasks_to_disk(tasks=tasks)
+    return True
+
+def remove_tasks(name):
+    tasks = get_tasks()
+    if name not in tasks:
+        api_logs.log_error(f"{name} not found")
+        return False
+
+    del tasks[name]
+    _write_tasks_to_disk(tasks=tasks)
+    return True
+
+def set_tasks_statut(name, done=True):
+    tasks = get_tasks()
+    if name not in tasks:
+        api_logs.log_error(f"{name} not found")
+        return False
+
+    tasks[name] = done
+    _write_tasks_to_disk(tasks=tasks)
+    return True
+
+def _write_tasks_to_disk(tasks):
+    if not os.path.exists(TASK_DIR):
+        os.makedirs(TASK_DIR)
+
+    with open(TASK_FILEPATH, "w") as f:
+        json.dump(tasks, f, indent=4)
+        api_logs.log_info(f"{tasks} removed")
+
+
+if __name__ == "__main__":
+    task = "Apprendre Pthon"
+    # add_tasks(task)
+    # set_tasks_statut(task)
+    remove_tasks(task)
+    # print(test_tache)
--- __5__Docstrings/__90__Projets/__17__PyTasks/.venv/main_window.py
+++ __5__Docstrings/__90__Projets/__17__PyTasks/.venv/main_window.py
@@ -11,22 +11,64 @@
 
 
 import sys
-from PySide6 import QtGui
-from PySide6.QtGui import QShortcut, QIcon
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QListWidget, QTextEdit, QGridLayout, QInputDialog, QListWidgetItem
+from PySide6 import QtGui, QtCore
+from PySide6.QtGui import QShortcut, QIcon, QColor
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QListWidget, QTextEdit, \
+    QVBoxLayout, QHBoxLayout, QLineEdit, QInputDialog, QListWidgetItem, QSystemTrayIcon
 import os
 
+from resources.constantes import COLORS
+from misc.logs import Logs
+# from misc.taskitem import TaskItem
+import API.task as api
 
-from Logs.logs import Logs
+class TaskItem(QListWidgetItem):
+
+    def __init__(self, name, done, list_widget):
+        super().__init__(name)
+        self.done = done
+        self.list_widget = list_widget
+        self.name = name
+
+        self.setSizeHint(QSize(self.sizeHint().width(), 100))
+        self.list_widget.addItem(self)
+        self.set_background_color()
+
+    def toggle_state(self):
+        self.done = not self.done
+        api.set_tasks_statut(name=self.name, done=self.done)
+        self.set_background_color()
+
+    def set_background_color(self):
+        colors = COLORS.get(self.done)
+        self.setBackground(QColor(*colors)) # * permet d'unpacker le tuple
+        color_str = ", ".join(map(str, colors))
+        stylesheet = f"""
+        QListView::item:selected {{
+            background-color: rgb({color_str});
+            color: rgb(0, 0, 0);
+        }}
+        QListView::item:hover {{
+            background-color: rgb(220, 220, 220);
+        }}
+        """
+        self.list_widget.setStyleSheet(stylesheet)
+
 
 class MainWindow(QWidget):
     def __init__(self, log_dir_name="Logs"):
         super().__init__()
-        self.logs = Logs(application_name="PyPlayer", log_dir=log_dir_name)
+        self.logs = Logs(application_name="PyTask", log_dir=log_dir_name)
         self.setWindowTitle("PyPlayer") # Changer le titre
-        self.setWindowIcon(QIcon("resources/icons/Icon.ico")) # Changer le lien
+
         self.current_dir = os.path.dirname(__file__)
+        self.resources_dir = os.path.join(self.current_dir, "resources")
+
+        self.setWindowIcon(QIcon("resources/base/icon.png"))  # Changer le lien
         self.setup_ui()
+        self.get_tasks()
+        self.center_under_tray()
 
     def setup_ui(self):
         """
@@ -38,6 +80,7 @@ class MainWindow(QWidget):
         try:
             self.create_widgets()
             self.create_layouts()
+            self.create_tray_icon()
             self.modify_widgets()
             self.add_widgets_to_layouts()
             self.setup_connections()
@@ -51,39 +94,116 @@ class MainWindow(QWidget):
         """
         Create widgets for the application
         """
-        pass
+        self.lw_tasks = QListWidget()
+        self.btn_add = QPushButton()
+        self.btn_clean = QPushButton()
+        self.btn_quit = QPushButton()
 
     def modify_widgets(self):
         """
         Show a style for the application
         """
-        pass
+        self.main_layout.setContentsMargins(5, 0, 5, 0)
+        self.main_layout.setSpacing(0)
+
+        self.setStyleSheet("border: none;")
+        # Qt.FramelessWindowHint: Enlève la barre des menus (réduire, fermer, minimiser)
+        # Qt.WindowStaysOnTopHint: Permet de mettre l'appli au 1ere plan
+        # | correspond à un "et"
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+
+        self.btn_add.setIcon(QIcon(os.path.join(self.resources_dir, "base/add.svg")))
+        self.btn_clean.setIcon(QIcon(os.path.join(self.resources_dir, "base/clean.svg")))
+        self.btn_quit.setIcon(QIcon(os.path.join(self.resources_dir, "base/close.svg")))
+
+        self.btn_add.setFixedSize(45, 45)
+        self.btn_clean.setFixedSize(45, 45)
+        self.btn_quit.setFixedSize(45, 45)
+
+        self.lw_tasks.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.lw_tasks.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def create_tray_icon(self):
+        """
+        Create the tray icon
+        # Icône se trouvant en bas à droite
+        """
+        self.tray = QSystemTrayIcon()
+        icon_path = os.path.join(self.resources_dir, "base/icon.png")
+        self.tray.setIcon(QIcon(icon_path))
+        self.tray.setVisible(True)
 
     def create_layouts(self):
         """
         Creation of grid layout
         """
-        pass
+        self.main_layout = QVBoxLayout(self)
+        self.layout_buttons = QHBoxLayout()
 
     def add_widgets_to_layouts(self):
         """
         Add widgets to the layout
         """
-        pass
+        self.main_layout.addWidget(self.lw_tasks)
+        self.main_layout.addLayout(self.layout_buttons)
+
+        self.layout_buttons.addWidget(self.btn_add)
+        self.layout_buttons.addStretch() # Ajoute un espace afin d'éloigner le bouton "add" et les boutons "clean" et "delete"
+        self.layout_buttons.addWidget(self.btn_clean)
+        self.layout_buttons.addWidget(self.btn_quit)
 
     def setup_connections(self):
         """
         Connect widgets to the methods
         """
-        pass
+        self.btn_add.clicked.connect(self.add_task)
+        self.btn_clean.clicked.connect(self.clean_task)
+        self.btn_quit.clicked.connect(self.close) # Fonction déjà intégré dans le QWidget
+        self.lw_tasks.itemClicked.connect(lambda lw_item: lw_item.toggle_state())
+        self.tray.activated.connect(self.tray_icon_click)
 
+    def add_task(self):
+        task_name, ok = QInputDialog.getText(self, "Ajouter une tâche", "Nom de la tâche:")
+        if ok and task_name:
+            api.add_tasks(name=task_name)
+            self.lw_tasks.addItem(task_name)
 
+    def clean_task(self):
+        for i in range(self.lw_tasks.count()):
+            lw_item = self.lw_tasks.item(i)
+            print(lw_item.done)
+            if lw_item.done:
+                api.remove_tasks(name=lw_item.name)
+            self.get_tasks()
+            self.lw_tasks.repaint()
 
+    def get_tasks(self):
+        self.lw_tasks.clear()
+        tasks = api.get_tasks()
+        for task_name, done in tasks.items():
+            TaskItem(name=task_name, done=done, list_widget=self.lw_tasks)
+
+    def tray_icon_click(self):
+        """
+        Hide/show the windows application
+        """
+        if self.isHidden():
+            self.showNormal()
+        else:
+            self.hide()
+
+    def center_under_tray(self):
+        tray_x = self.tray.geometry().x()
+        tray_y = self.tray.geometry().y()
+        print(tray_x, tray_y)
+        w, h = self.sizeHint().toTuple() # Taille de l'appli
+        print(w, h)
+        self.move(tray_x-w, tray_y-h-100)
 if __name__ == "__main__":
     app = QApplication() # 1. Instantiate QApplication
     windows = MainWindow(log_dir_name="Test_log")
     windows.show()
-    exit_app_logs = Logs(application_name="PyPlayer", log_dir="Test_log") # Changer le titre
+    exit_app_logs = Logs(application_name="PyTask", log_dir="Test_log") # Changer le titre
 
     exit_code = app.exec() # 2. Invoke app.exec()
     exit_app_logs.log_info("Application is shutting down")
